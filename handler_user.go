88c3c1f574f67bc1069bcef8619230384f638bf0
Jerry Chien <chiungkunchien@gmail.com>
2023-12-14 05:08:42 +0000

    create Authorization API Key

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/GoJerryChien/rss_agg_go/internal/auth"
 	"github.com/GoJerryChien/rss_agg_go/internal/database"
 	"github.com/google/uuid"
 )
@@ -34,5 +35,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		repondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		repondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
 	respondWithJSON(w, 200, databaseUserToUser(user))
+
 }
